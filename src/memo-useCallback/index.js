84a96622b9e3f282bbac05f4ba03b6e0b294236f
Thiliban <thilip2017@gmail.com>
2022-11-23 19:09:46 +0530

    usecallback hooks added

--- src/memo-useCallback/index.js
+++ src/memo-useCallback/index.js
@@ -5,10 +5,14 @@ const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
 
-
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
 
   return (
     <>
@@ -16,25 +20,34 @@ const Index = () => {
       <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+
+      <h1 style={{ marginTop: "3rem" }}>Cart :{cart}</h1>
+
+      <BigList products={products} addToCart={addToCart} />
     </>
   );
 };
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   useEffect(() => {
     console.log("Big list called");
   });
   return (
     <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>;
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
   );
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(() => {
     console.count("Single item called");
   });
@@ -47,6 +60,9 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button className="btn" onClick={addToCart}>
+        Add to cart
+      </button>
     </article>
   );
 };
